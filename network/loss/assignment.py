927ed5a2a18aff960cd9e5baac7ffecae2d1f364
tihsu99 <b07202037@ntu.edu.tw>
2026-06-23 17:23:21 +0200

    refactor detection metrics and visualization to enhance summary and plotting functionality

--- network/loss/assignment.py
+++ network/loss/assignment.py
@@ -293,12 +293,15 @@ def loss_single_process(
     ## Detection Loss ##
     ####################
 
-    particle_balance_weight = torch.ones_like(targets_mask[0], dtype=torch.float32)
+    base_particle_balance_weight = torch.ones_like(targets_mask[0], dtype=torch.float32)
     masks_for_balance = torch.stack(targets_mask).int()
 
     if particle_index_tensor is not None:
         class_indices = (masks_for_balance * particle_index_tensor.to(masks_for_balance.device).unsqueeze(1)).sum(0).int()
-        particle_balance_weight *= particle_weights_tensor.to(masks_for_balance.device)[class_indices]
+        base_particle_balance_weight = (
+                base_particle_balance_weight *
+                particle_weights_tensor.to(masks_for_balance.device)[class_indices]
+        )
 
     detections = detections
     detections_target = targets_mask
@@ -329,15 +332,15 @@ def loss_single_process(
 
     process_masking = torch.stack(process_masking).float()
     process_weighting = torch.stack(process_weighting).float()
-    particle_balance_weight = particle_balance_weight.unsqueeze(0)
+    detection_particle_balance_weight = base_particle_balance_weight.unsqueeze(0)
 
     if event_weight is not None:
         event_balance_weight = event_weight.view(-1, *([1] * (process_masking.dim() - 1)))
-        detection_losses = torch.stack(detection_losses) * process_masking * process_weighting * event_balance_weight * particle_balance_weight
-        valid_process = torch.sum(process_masking * process_weighting * event_balance_weight * particle_balance_weight)
+        detection_losses = torch.stack(detection_losses) * process_masking * process_weighting * event_balance_weight * detection_particle_balance_weight
+        valid_process = torch.sum(process_masking * process_weighting * event_balance_weight * detection_particle_balance_weight)
     else:
-        detection_losses = torch.stack(detection_losses) * process_masking * process_weighting * particle_balance_weight
-        valid_process = torch.sum(process_masking * process_weighting * particle_balance_weight)
+        detection_losses = torch.stack(detection_losses) * process_masking * process_weighting * detection_particle_balance_weight
+        valid_process = torch.sum(process_masking * process_weighting * detection_particle_balance_weight)
 
     if valid_process > 0:
         detection_loss = torch.sum(detection_losses) / valid_process
@@ -360,10 +363,10 @@ def loss_single_process(
         focal_gamma
     )
 
-    particle_balance_weight = particle_balance_weight.squeeze(0).to(symmetric_losses.device)
+    particle_balance_weight = base_particle_balance_weight.to(symmetric_losses.device)
 
     if process_weight[0] is not None:
-        particle_balance_weight *= process_weight[0]
+        particle_balance_weight = particle_balance_weight * process_weight[0]
 
     targets_mask_finite = torch.stack(targets_mask).float()
     if not torch.isfinite(symmetric_losses).all():
